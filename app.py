90343e648c14536f8e0a708de881f9c182282ad9
YaseenKhan1209 <111399262+YaseenKhan1209@users.noreply.github.com>
2024-12-30 06:09:27 +0000

    data.db and flask

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, flash, abort, url_for
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import sys
+import os
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
